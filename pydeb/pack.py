d2a14d39f840e3c85cf38468a353fc43e8952dee
jaidan <ajaidan0@gmail.com>
2023-01-28 21:15:00 -0500

    fix (PACK): fix up ar

--- pydeb/pack.py
+++ pydeb/pack.py
@@ -1,8 +1,9 @@
 # module imports
 import tarfile
-from os import listdir, path, system
+from os import listdir, path
 from pathlib import Path
 from shutil import copytree, rmtree
+from subprocess import getoutput
 
 # local imports
 from .control import Control
@@ -10,7 +11,9 @@ from .utils import cmd_in_path
 
 
 class Pack:
-	def __init__(self, path: str, algorithm: str = 'xz', compression_level: int = 9):
+	def __init__(self, path: Path, algorithm: str = 'xz', compression_level: int = 9):
+		# check if path is Path
+		if type(path) is not Path: path = Path(path)
 		# path to pack
 		self.path = path
 		# algo
@@ -24,14 +27,14 @@ class Pack:
 
 	def __pack(self):
 		# check if path exists
-		if not path.exists(self.path):
+		if not self.path.exists():
 			raise Exception(f'Passed file {self.path} does not exist.')
 		# check if path is correct
-		if not path.isdir(self.path):
+		if not self.path.is_dir():
 			raise Exception(
 				f'Passed file {self.path} is not a directory file.')
 		# check if DEBIAN exists
-		if not path.exists(f'{self.path}/DEBIAN/control'):
+		if not Path(f'{self.path}/DEBIAN/control').exists():
 			raise Exception(f'DEBIAN/control does not exist.')
 		# get ar command
 		ar = cmd_in_path('ar')
@@ -40,8 +43,7 @@ class Pack:
 			raise Exception(
 				'Command "ar" is not installed. Please install it in order to use this library.')
 		# ensure algorithm is valid
-		if self.algorithm != 'xz' and self.algorithm != 'gzip' and self.algorithm != 'bzip2':
-			print(self.algorithm)
+		if self.algorithm not in ['xz', 'gzip', 'bzip2']:
 			raise Exception(
 				f'Invalid algorithm type {self.algorithm}. Valid types are: xz, gzip, bzip2. Default is xz.')
 		# ensure compression level is valid
@@ -49,14 +51,13 @@ class Pack:
 			raise Exception(
 				f'Invalid compression level {self.level}. Valid levels are 1-9. Default is 9.')
 		# formatted path
-		fpath = Path(self.path)
 		with open(f'{self.path}/DEBIAN/control', 'r') as f:
 			control = Control(f.read())
 			self.debpath = f'{control.package}_{control.version}_{control.architecture}.deb'
 		# tmp dir
-		tmp = f'.{fpath.name}.pack.tmp'
+		tmp = Path(f'.{self.path.name}.pack.tmp')
 		# remove tmp dir if it exists
-		if path.exists(tmp):
+		if tmp.exists():
 			rmtree(tmp)
 		# loop through files
 		for f in listdir(self.path):
@@ -74,28 +75,28 @@ class Pack:
 		# this is needed due to a strange issue where xz compression has preset instead of compresslevel.
 		if self.algorithm == 'xz':
 			# create control archive
-			with tarfile.open(f'{tmp}/control.tar.{self.algorithm}', f'w:{self.algorithm}', preset=self.level) as tar:
-				tar.add(f'{tmp}/DEBIAN', arcname='/')
+			with tarfile.open(f'{tmp}/control.tar.{self.algorithm}', f'w:{self.algorithm}', preset=self.level, format=tarfile.GNU_FORMAT) as tar:
+				tar.add(f'{tmp}/DEBIAN', arcname='.')
 				tar.close()
 
 			# create data archive
-			with tarfile.open(f'{tmp}/data.tar.{self.algorithm}', f'w:{self.algorithm}', preset=self.level) as tar:
-				tar.add(f'{tmp}/FILESYSTEM', arcname='/')
+			with tarfile.open(f'{tmp}/data.tar.{self.algorithm}', f'w:{self.algorithm}', preset=self.level, format=tarfile.GNU_FORMAT) as tar:
+				tar.add(f'{tmp}/FILESYSTEM', arcname='.')
 				tar.close()
 		else:
 			# create control archive
-			with tarfile.open(f'{tmp}/control.tar.{self.algorithm}', f'w:{self.algorithm}', compresslevel=self.level) as tar:
-				tar.add(f'{tmp}/DEBIAN', arcname='/')
+			with tarfile.open(f'{tmp}/control.tar.{self.algorithm}', f'w:{self.algorithm}', compresslevel=self.level, format=tarfile.GNU_FORMAT) as tar:
+				tar.add(f'{tmp}/DEBIAN', arcname='.')
 				tar.close()
 
 			# create data archive
-			with tarfile.open(f'{tmp}/data.tar.{self.algorithm}', f'w:{self.algorithm}', compresslevel=self.level) as tar:
-				tar.add(f'{tmp}/FILESYSTEM', arcname='/')
+			with tarfile.open(f'{tmp}/data.tar.{self.algorithm}', f'w:{self.algorithm}', compresslevel=self.level, format=tarfile.GNU_FORMAT) as tar:
+				tar.add(f'{tmp}/FILESYSTEM', arcname='.')
 				tar.close()
 
 		# create binary
 		with open(f'{tmp}/debian-binary', 'w') as f:
-			f.write('2.0')
+			f.write('2.0\n')
 			f.close()
 
 		# delete paths
@@ -103,7 +104,7 @@ class Pack:
 		rmtree(f'{tmp}/FILESYSTEM')
 
 		# create deb
-		system(f'{ar} r {self.debpath} {tmp}/* >/dev/null 2>&1')
+		getoutput(f'{ar} r {self.debpath} {tmp}/debian-binary {tmp}/control.tar.* {tmp}/data.tar.*')
 
 		# delete tmp
 		rmtree(tmp)
